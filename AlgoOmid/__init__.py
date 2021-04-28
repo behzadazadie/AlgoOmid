886376eca9135f686cc0ab8b8fa6e72ac92e9f32
b.azadi <b.azadi@omid.ir>
2021-04-28 13:51:23 +0430

    editing the init file

--- AlgoOmid/__init__.py
+++ AlgoOmid/__init__.py
@@ -1,9 +1,2 @@
-from AlgoOmid.AlgorithmTrading_Module import Create_isin_object
-from AlgoOmid.AlgorithmTrading_Module import TSE_Close_Daily
-from AlgoOmid.AlgorithmTrading_Module import dataengine_Data
-from AlgoOmid.AlgorithmTrading_Module import Get_SW_Data
-from AlgoOmid.AlgorithmTrading_Module import Get_BidAsk_Data
-from AlgoOmid.AlgorithmTrading_Module import is_SW_online
-from AlgoOmid.AlgorithmTrading_Module import is_trade_time
-from AlgoOmid.AlgorithmTrading_Module import Send_Order
-
+from AlgoOmid.AlgorithmTrading_Module import Create_isin_object, TSE_Close_Daily, dataengine_Data, Get_SW_Data, 
+Get_BidAsk_Data, is_SW_online, is_trade_time, Send_Order
